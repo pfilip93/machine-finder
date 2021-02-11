92072192995404e0fd0d96eda0232afed2f437d5
Piotr Filipowicz <p.filip93@gmail.com>
2021-02-11 13:26:38 +0100

    style: close icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./App.scss";
 
+const CloseIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="1em"
+    height="1em"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+  </svg>
+);
+
 export default function App() {
   const [open, setOpen] = React.useState(false);
   const [name, setName] = React.useState("");
@@ -23,7 +35,7 @@ export default function App() {
               aria-label="Close"
               onClick={() => setOpen(false)}
             >
-              x
+              <CloseIcon />
             </button>
           </div>
 
