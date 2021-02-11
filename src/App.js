be6b7d30661d2b3e0251d5f0cdf51fad3100216d
Piotr Filipowicz <p.filip93@gmail.com>
2021-02-11 13:03:54 +0100

    feat: display modal on click button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.scss";
 
-function App() {
+export default function App() {
+  const [open, setOpen] = React.useState(false);
+  const [name, setName] = React.useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>klickrent</h1>
       </header>
+      <main>
+        <button onClick={() => setOpen(true)}>Start inquiry</button>
+      </main>
+
+      {open && (
+        <div className="modal">
+          <div className="topBar">
+            <h2>Direct request</h2>
+            <button
+              className="close"
+              aria-label="Close"
+              onClick={() => setOpen(false)}
+            >
+              x
+            </button>
+          </div>
+
+          <input
+            className="name"
+            type="text"
+            placeholder="Machine name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+
+          <div className="list">
+            <h3>Category #1</h3>
+            <ul>
+              <li>Lorem ipsum</li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
